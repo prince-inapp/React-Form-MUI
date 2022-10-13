cfd475d175b31c23b0b86542b548de9983b30ccc
Prince Johnson <prince.j@inapp.com>
2022-10-13 17:32:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Form from './components/Form';
+import Users from './components/Users';
+import {useState} from 'react';
 function App() {
+  const [ data, setData ] = useState([]);
+
+  function addToDataHandler(newData){
+    let id = data.length + 1;
+    setData((prevState)=>{
+      return [...prevState,{ 'id' : id  , 'name': newData.name, 'email': newData.email, 'phone': newData.phone}];
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form addToData = {addToDataHandler}></Form>
+      <Users userList = {data}/>
     </div>
   );
 }
